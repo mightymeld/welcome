cb3135744c8fe11331933bbae3d11135b3b5c87a
Daniel Worthington <daniel@halffullheart.com>
2024-01-12 13:18:48 -0800

    Prettier

--- src/tutorial.jsx
+++ src/tutorial.jsx
@@ -2,11 +2,9 @@ import { useMatches } from "react-router-dom";
 import { steps } from "./instructions";
 
 export const Tutorial = () => {
-    const [match] = useMatches();
-    const step = match.params?.step ? parseInt(match.params.step, 10) : 0;
-    const InstructionsForStep = steps[step];
-  
-    return (
-      <InstructionsForStep />
-    )
-  };
+  const [match] = useMatches();
+  const step = match.params?.step ? parseInt(match.params.step, 10) : 0;
+  const InstructionsForStep = steps[step];
+
+  return <InstructionsForStep />;
+};
